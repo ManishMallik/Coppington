bc2a2f8bd8c31405d847efc2b729c3a55ca67deb
iamudyavar <aditya.udyavar@gmail.com>
2023-11-04 17:48:27 -0500

    Added backend MongoDB integration

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,44 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson.json_util import dumps
 
+###################
+# API Setup
+###################
+# Set up Flask API
 api = Flask(__name__)
 CORS(api)
 
+# Set up MongoDB connection
+uri = "mongodb+srv://admin:CuPeBtnHjDuUGchX@coppingtoncluster.7xxwfc0.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi("1"))
 
-@api.route("/")
-def my_profile():
-    response_body = {
-        "message": "Hello world!",
-    }
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
+# Set database
+database = client["ListingsDatabase"]["ListingsCollection"]
+
+
+###################
+# API Starts Here
+###################
+@api.route("/api/getListings", methods=["GET"])
+def get_listings():
+    listings = database.find_one()
+    print(listings)
+
+    listingsJSON = dumps(listings)
+    print(listingsJSON)
+
+    # Return database entries
+    response_body = listingsJSON
     return response_body
 
 
