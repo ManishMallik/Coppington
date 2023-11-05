89a1af4cde60d5b24d23fc3232c776153ae2ce6b
iamudyavar <aditya.udyavar@gmail.com>
2023-11-04 21:41:23 -0500

    Adding backend integration

--- frontend/src/components/Listing.js
+++ frontend/src/components/Listing.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
@@ -9,48 +10,57 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const listings = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Listing() {
-  return (
-    <>
-      <CssBaseline />
-      <main>
+    useEffect(() => {
+        fetch("http://localhost:8000/api/getListings").then(
+          response => response.text()
+        ).then(
+          data => {
+            console.log(data)
+          }
+        )
+    }, [])
+
+    return (
+        <>
+        <CssBaseline />
+        <main>
         <Container sx={{ py: 8 }}>
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+        <Grid container spacing={4}>
+        {listings.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+            <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+            >
+                <CardMedia
+                component="div"
+                sx={{
+                    // 16:9
+                    pt: '56.25%',
+                }}
+                image="https://source.unsplash.com/random?wallpapers"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                    Heading
+                </Typography>
+                <Typography>
+                    This is a media card. You can use this section to describe the
+                    content.
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="small">View</Button>
+                <Button size="small">Edit</Button>
+                </CardActions>
+            </Card>
+            </Grid>
+        ))}
+        </Grid>
         </Container>
-      </main>
-      </>
+        </main>
+    </>
   );
 }
